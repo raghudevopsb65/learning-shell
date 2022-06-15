6df2c14dfb9b203e43fe9d3fc45ee12da0ff6aa8
Raghu K <raghuk.vit@gmail.com>
2022-06-15 06:52:33 +0530

    Print demo

--- 02-print.sh
+++ 02-print.sh
@@ -11,3 +11,6 @@ echo Hello,How are you doing today, Good day
 
 # syntax: echo -e "line1 \n line2"
 echo -e "Hello,\nHow are you doing today.\nGood day"
+
+# Print a tab space using \t
+echo -e "a\tb"
