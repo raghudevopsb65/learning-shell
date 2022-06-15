1e7c24ba3472e853226f610364a701eba89606db
Raghu K <raghuk.vit@gmail.com>
2022-06-15 06:56:52 +0530

    Print demo

--- 02-print.sh
+++ 02-print.sh
@@ -14,3 +14,22 @@ echo -e "Hello,\nHow are you doing today.\nGood day"
 
 # Print a tab space using \t
 echo -e "a\tb"
+
+# Print some colors
+# syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# -e -> enable esc seq
+# \e -> enable col
+# [COLm -> GIve color input
+# \e[0m -> Disable the color
+
+# What are the colors we have ?
+# COL           CODE      SYNTAX
+# RED           31          \e[31m
+# GREEN         32          \e[32m
+# YELLOW        33          \e[33m
+# BLUE          34          \e[34m
+# MAGENTA       35          \e[35m
+# CYAN          36          \e[36m
+
+echo -e "\e[31mHello in RED Color"
+echo "Hello in Normal text color"
